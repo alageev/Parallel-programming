cf482de006206be383c97f756d1fef7157b9f390
alageev <alageev01@gmail.com>
2021-02-12 10:26:33 +0300

    Initial Commit

---
+++ Parallel programming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Parallel programming
+//
+//  Created by Алексей Агеев on 12.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
